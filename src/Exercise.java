66de3c360ef9d9862868e3e8914ad2887addc40c
Nilam <nilammartaanggreni@gmail.com>
2024-10-24 17:56:40 +0800

    exercise chapter 1

---
+++ src/Exercise.java
@@ -0,0 +1,42 @@
+// a
+
+//public class Exercise {
+//    public static void main(String[] args) {
+//        int x = 1;
+//        while (x < 10) {
+//            if (x > 3) {
+//                System.out.println("big x");
+//            }
+//            x = x + 1;
+//        }
+//    }
+//}
+
+// b
+
+//public static void main(String[] args) {
+//    int x = 5;
+//    while (x > 1) {
+//        if (x < 3) {
+//            System.out.println("small x");
+//        }
+//        x = x - 1;
+//    }
+//}
+
+
+// c
+class Exercise {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("small x");
+            }
+        }
+    }
+}
+
+
+
