e13d71dff0fd4952e623618cbf1b6005699ea558
Nilam <nilammartaanggreni@gmail.com>
2024-11-01 18:04:41 +0800

    exercise chapter 3

--- src/Exercise3/Triangle.java
+++ src/Exercise3/Triangle.java
@@ -5,23 +5,30 @@ public class Triangle {
     int height;
     int length;
 
-    void setArea(){
-        area = (height * length) / 2;
+    void setArea() {
+        area = (double) (height * length) / 2;
     }
 
     public static void main(String[] args) {
-            Triangle [] ta = new Triangle[4];
+        Triangle[] ta = new Triangle[4];
 
-            int x = 0;
-            while (x < 4){
-                ta[x] = new Triangle();
-                ta[x].height = (x + 1) * 2;
-                ta[x].length = x + 4;
-                ta[x].setArea();
+        int x = 0;
+        while (x < 4) {
+            ta[x] = new Triangle();
+            ta[x].height = (x + 1) * 2;
+            ta[x].length = x + 4;
+            ta[x].setArea();
 
-                System.out.print("triangle" + x + ", area");
-                System.out.println(" = " + ta[x].area);
-                
-            }
+            System.out.print("triangle" + x + ", area");
+            System.out.println(" = " + ta[x].area);
+
+            x = x + 1;
+        }
+
+        int y = x;
+        x = 27;
+        Triangle t5 = ta[2];
+        System.out.print("y = " + y);
+        System.out.print(", t5 area = " + t5.height);
     }
 }
